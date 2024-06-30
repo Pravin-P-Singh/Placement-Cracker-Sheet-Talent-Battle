619fde2328c8d7f739c04f993d1f54c4525cb2d5
Pravin Singh <pravin130204@gmail.com>
2024-06-30 23:56:33 +0530

    Day 2 completed

---
+++ Day 2/PS3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PS3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next().toLowerCase();
+        char ch = input.charAt(0);
+
+        if(ch>='a' && ch <='z'){
+            System.out.println("Alphabet");
+        } else{
+            System.out.println("Not an alphabet");
+        }
+    }
+}
