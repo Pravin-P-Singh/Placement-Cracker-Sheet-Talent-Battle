e56094bbb77155a95cfcc83ca6615959950e7342
Pravin Singh <pravin130204@gmail.com>
2024-07-01 23:52:43 +0530

    Day 5 one problem statment done

---
+++ Day 3/PS5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PS5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        if(num1>0 && num2>0){
+            System.out.println("The point lies in first quadrant");
+        }
+        else if(num1<0 && num2>0){
+            System.out.println("The point lies in second quadrant");
+        }
+        else if(num1<0 && num2<0){
+            System.out.println("The point lies in third quadrant");
+        } else{
+            System.out.println("The point lies in fourth quadrant");
+        }
+        sc.close();
+    }    
+}
