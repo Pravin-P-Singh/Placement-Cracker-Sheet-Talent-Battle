7dc56b803ff2141a0b224096f972fae8e80f657c
Pravin Singh <pravin130204@gmail.com>
2024-07-02 00:00:21 +0530

    Day 3 partially done

--- Day 3/PS6.java
+++ Day 3/PS6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PS5 {
+public class PS6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
