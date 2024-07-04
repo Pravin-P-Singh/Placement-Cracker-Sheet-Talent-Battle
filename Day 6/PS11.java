65bb0b5451f4e31ba861b80579c1e16062bf22d1
Pravin Singh <pravin130204@gmail.com>
2024-07-05 00:15:06 +0530

    Day 5 one problem statment done

--- Day 6/PS11.java
+++ Day 6/PS11.java
@@ -6,14 +6,15 @@ public class PS11{
         int num = sc.nextInt();
         fibonacci(num);
         sc.close();
-    }    
-    public static void fibonacci (int num){
-        int a=0,b=1,temp;
-        for(int i = 0; i < num; i++){
-            temp = a+b;
-            b=a+b;
-            System.out.print(b+" ");
-            a = b;
-        }    
+    }
+
+    public static void fibonacci(int num) {
+        int a = 0, b = 1, temp = 0;
+        for (int i = 0; i < num; i++) {
+            temp = b;
+            b = a + b;
+            System.out.print(a + " ");
+            a = temp;
+        }
     }
 }
