c729490d1286b1ad693e873dd308d8589c5038e6
Pravin Singh <pravin130204@gmail.com>
2024-07-15 20:12:53 +0530

    Day 14 2 problem statements done

---
+++ Day 14/PS27.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+
+public class PS27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int ans = input << 1;
+        System.out.println(ans);
+        sc.close();
+    }
+}
---
+++ Day 14/PS28.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PS28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        for(int i = input.length()-1; i>=0; i--){
+            System.out.print(input.charAt(i));
+        }
+        sc.close();
+    }    
+}
